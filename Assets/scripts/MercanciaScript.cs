78f8e858ae27460d70182b595c0f9aa4bdddaf49
Iair1 <166546999+Iair1@users.noreply.github.com>
2025-11-18 20:16:12 -0300

    Initial commit

---
+++ Assets/scripts/MercanciaScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+    public int precio;
+    public moneyManager moneyManager;
+    void Start()
+    {
+        moneyManager = FindObjectOfType<moneyManager>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "player")
+        {
+            if(moneyManager.UpdateMoney(-precio) == true) { 
+            Destroy(gameObject);
+            }
+        }
+    }
+}
