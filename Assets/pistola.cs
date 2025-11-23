a06c08ecad0ddac5b98b4f9a20797e58ac0362be
Iair1 <166546999+Iair1@users.noreply.github.com>
2025-11-23 19:38:00 -0300

    n

---
+++ Assets/pistola.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pistola : MonoBehaviour
+{
+    [SerializeField] Transform firePoint;
+    [SerializeField] GameObject bullet;
+    [SerializeField] float shootingForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            GameObject bulletClone = Instantiate(bullet, firePoint.position, transform.rotation);
+            bulletClone.GetComponent<Rigidbody>().AddForce(transform.forward * shootingForce, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Assets/zombieHealthAndEnemyManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zombieHealthAndEnemyManager : MonoBehaviour
+{
+    [SerializeField] int vidaZ;
+    [SerializeField] Transform bala;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vidaZ = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.transform == bala)
+        {
+            Debug.Log("Choqueee");
+            vidaZ -= 33;
+            Destroy(other.gameObject);
+            if(vidaZ<=0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
