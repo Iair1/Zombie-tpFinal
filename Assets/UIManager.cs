3136d64058c9beb03c117250b0587696e959b4ce
Iair1 <166546999+Iair1@users.noreply.github.com>
2025-11-24 17:25:56 -0300

    n

---
+++ Assets/UIManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+    [SerializeField] GameObject cosoDeLaE;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    public void muestraAgarraPistola()
+    {
+        cosoDeLaE.SetActive(true);
+    }
+    public void noMuestraAgarraPistola()
+    {
+        cosoDeLaE.SetActive(false);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/activator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activator : MonoBehaviour
+{
+    [SerializeField] GameObject pistolaEnMano;
+    [SerializeField] GameObject pistolaEnSuelo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pistolaEnMano.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if(other.transform = pistolaEnSuelo)
+        {
+            UIManager.muestraAgarraPistola();
+        }
+    }
+}
