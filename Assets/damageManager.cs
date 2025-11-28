c98225e4aac0980be784bd2927db2636286a1816
Iair1 <166546999+Iair1@users.noreply.github.com>
2025-11-28 00:04:01 -0300

    Update damageManager.cs

--- Assets/damageManager.cs
+++ Assets/damageManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class damageManager : MonoBehaviour
@@ -7,10 +8,12 @@ public class damageManager : MonoBehaviour
     [SerializeField] Collider zombie1;
     [SerializeField] Collider zombie2;
     [SerializeField] int vida;
+    [SerializeField] bool tocadoHacePoco;
     // Start is called before the first frame update
     void Start()
     {
         vida = 100;
+        tocadoHacePoco = false;
     }
 
     // Update is called once per frame
@@ -20,15 +23,25 @@ public class damageManager : MonoBehaviour
     }
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log(zombie1);
         if(other == zombie1 || other== zombie2)
         {
-            vida -= 33;
-            UIManager.Instance.updateVida(vida);
-            if(vida<=0)
-            {
-                UIManager.Instance.derrota();
+            if(!tocadoHacePoco){
+                inmunidad();
+                 vida -= 33;
+                UIManager.Instance.updateVida(vida);        
+                if(vida<=0)
+                {
+                    UIManager.Instance.derrota();
+                }
             }
         }
+    }
+    private async void inmunidad()
+    {
+        
+            tocadoHacePoco = true;
+            await Task.Delay(2000);
+            tocadoHacePoco = false;
+   
     }
 }
