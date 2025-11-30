610272f3e99222a80703a8780f455abb9ff3320a
Iair1 <166546999+Iair1@users.noreply.github.com>
2025-11-30 00:20:51 -0300

    terminado

--- Assets/scripts/UIManager.cs
+++ Assets/scripts/UIManager.cs
@@ -10,8 +10,25 @@ public class UIManager : MonoBehaviour
     [SerializeField] GameObject pantallaPerdio;
     [SerializeField] GameObject noPerdio;
     [SerializeField] GameObject pantallaVictoria;
+    [SerializeField] GameObject pantallaTutorial;
+    [SerializeField] GameObject avanzatxt;
+    [SerializeField] GameObject retrocedetxt;
+    [SerializeField] TextMeshProUGUI texto;
     [SerializeField] TextMeshProUGUI vidaTxt;
     [SerializeField] bool perdio;
+    [SerializeField] bool tutorial;
+    [SerializeField] int indiceActual;
+    [SerializeField] int txtCantidad;
+    [SerializeField] List<string> nombresTutoriales = new List<string>()
+    {
+        "Bienvenido a Zombie Hunter",
+        "En este juego, deberás buscar un arma para destruir a los zombies", 
+        "Durante la primera parte, deberás eludirlos para conseguir el arma",
+        "Sin ella, no podrás avanzar a la segunda parte",
+        "Al llegar a la segunda parte, apareceran zombies que deberás destruir para ganar",
+        "Pero cuidado, estarás casi completamente a oscuras, y las balas no llegan muy lejos",
+        "¡Buena Suerte!"
+    };
     // Start is called before the first frame update
     private void Awake()
     {
@@ -47,16 +64,53 @@ public class UIManager : MonoBehaviour
 
     void Start()
     {
-        Time.timeScale = 1;
-        noPerdio.SetActive(true);
+        //Time.timeScale = 1;
+        //noPerdio.SetActive(true);
+        Time.timeScale = 0;
         pantallaPerdio.SetActive(false);
         pantallaVictoria.SetActive(false);
+        pantallaTutorial.SetActive(true);
+        avanzatxt.SetActive(true);
+        retrocedetxt.SetActive(false);
         perdio = false;
+        tutorial = true;
+        indiceActual = 0;
+        txtCantidad = 7;
+        texto.text = nombresTutoriales[0];
     }
-
     // Update is called once per frame
     void Update()
     {
+    
+        if(Input.GetKeyDown(KeyCode.S) && tutorial == true)
+        {
+            Time.timeScale = 1;
+            noPerdio.SetActive(true);
+            pantallaTutorial.SetActive(false);
+
+        }
+        if(Input.GetKeyDown(KeyCode.N) && tutorial == true && indiceActual != txtCantidad-1)
+        {
+            indiceActual += 1;
+            texto.text = nombresTutoriales[indiceActual];
+            Debug.Log(nombresTutoriales[indiceActual]);
+            if(indiceActual == txtCantidad-1){
+                avanzatxt.SetActive(false);
+            } else if(indiceActual == 1){
+                retrocedetxt.SetActive(true);
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.P) && tutorial == true && indiceActual != 0)
+        {
+            indiceActual -= 1;
+            texto.text = nombresTutoriales[indiceActual];
+            if(indiceActual == txtCantidad-2){
+                avanzatxt.SetActive(true);
+            } else if(indiceActual == 0){
+                retrocedetxt.SetActive(false);
+            }
+        }
+
         if (Input.GetKeyDown(KeyCode.Z) && perdio == true)
         {
             SceneManager.LoadScene("SampleScene");
---
+++ Assets/scripts/spawneador.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawneador : MonoBehaviour
+{
+    [SerializeField] GameObject Zombies;
+    [SerializeField] GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Zombies.SetActive(false);
+        player = GameObject.Find("SimpleFPSController"); 
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            Zombies.SetActive(true);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
